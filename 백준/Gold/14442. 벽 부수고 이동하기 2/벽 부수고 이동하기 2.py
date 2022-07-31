db406dbe2ab09d924a07f0af9a619459a1b1aed0
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-07-31 18:07:57 +0900

    Update 벽 부수고 이동하기 2.py

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,30 +1,32 @@
 from collections import deque
-q = deque()
-from sys import stdin
-input = stdin.readline
+import sys
+input = sys.stdin.readline
+queue = deque()
 
-n,m,k = map(int, input().split())
-vis = [[[0]*(k+1) for _ in range(m)] for __ in range(n)]
-arr = [list(map(int,input().strip())) for _ in range(n)]
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+N,M,K = map(int, input().split())
+visited = [[[0]*(K+1) for _ in range(M)] for _ in range(N)]    #각 레이어마다 방문여부 저장리스트
+graph = [list(map(int,input().strip())) for _ in range(N)]
+dr = [-1,1,0,0]    #상하좌우
+dc = [0,0,-1,1]
 
 def bfs() :
-    q.append([0,0,k]) # k는 벽을 뚫을 수 있는 수
-    vis[0][0][k] = 1
-    while q :
-        x,y,z = q.popleft()
-        if x == n-1 and y == m-1 :
-            return vis[x][y][z]
-        for i in range(4) :
-            nx ,ny = dx[i] + x, dy[i]+y
-            if 0<=nx<n and 0<=ny<m :
-                if arr[nx][ny]==1 and z>0 and vis[nx][ny][z-1]==0:
-                    vis[nx][ny][z-1] = vis[x][y][z]+1
-                    q.append([nx,ny,z-1])
-                elif arr[nx][ny]==0 and vis[nx][ny][z]==0:
-                    vis[nx][ny][z] = vis[x][y][z]+1
-                    q.append([nx,ny,z]) 
-    return -1
+    queue.append([0,0,K])
+    visited[0][0][K] = 1    #시작점 방문처리
+    while queue :
+        x,y,z = queue.popleft()
+        if x == N-1 and y == M-1 :    #도착시에 return
+            return visited[x][y][z]
+        for i in range(4):
+            nx = dr[i] + x
+            ny = dc[i] + y
+            if 0<=nx<N and 0<=ny<M :    #범위안에있고
+                # 벽이고 and 벽을 부술 수 있고 and 부순 벽에있는 레이어를 방문한적 없으면
+                if graph[nx][ny]==1 and z>0 and visited[nx][ny][z-1]==0:
+                    visited[nx][ny][z-1] = visited[x][y][z]+1    #한 번 벽을 부순 레이어에 방문횟수 추가
+                    queue.append([nx,ny,z-1])    #z-1 -> 한 번 벽을 부순 레이어로 이동
+                elif graph[nx][ny]==0 and visited[nx][ny][z]==0:    #통과가능하고 방문한적이 없다면
+                    visited[nx][ny][z] = visited[x][y][z]+1
+                    queue.append([nx,ny,z])
+    return -1    #도착못할시에 return -1
 
 print(bfs())

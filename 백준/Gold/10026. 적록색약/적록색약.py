7a411658148f62c681c4a13141a38f9fd858d5d3
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-01 13:42:28 +0900

    [Gold V] Title: 적록색약, Time: 196 ms, Memory: 117592 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -6,7 +6,7 @@ N = int(input())
 graph = [list(input().rstrip()) for _ in range(N)]
 queue = deque()
 
-dr = [-1,1,0,0]
+dr = [-1,1,0,0]    #4방향
 dc = [0,0,-1,1]
 
 def bfs(r,c,s):
@@ -19,29 +19,22 @@ def bfs(r,c,s):
         for i in range(4):
             nx = x + dr[i]
             ny = y + dc[i]
-
+            #범위를 벗어나지 않고 방문한적이 없다면
             if 0<=nx<N and 0<=ny<N and not visited[nx][ny]:
-                if graph[nx][ny] == s:
+                if graph[nx][ny] == s:    #해당 index 색 = 탐색하고자 하는 색
                     visited[nx][ny] = True
                     queue.append([nx,ny])
-    result+=1
+    result+=1    #한 구간 탐색 후 증감
 
-result = 0
-visited = [[False for _ in range(N)] for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]:
-            bfs(i,j,graph[i][j])
-print(result)
-
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 'G':
-            graph[i][j] = 'R'
-result = 0
-visited = [[False for _ in range(N)] for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]:
-            bfs(i,j,graph[i][j])
-print(result)
+for i in range(2):
+    result = 0
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                bfs(i,j,graph[i][j])
+    print(result)
+    for i in range(N):    #적록색약 R==G -> G->R로 바꾸기
+        for j in range(N):
+            if graph[i][j] == 'G':
+                graph[i][j] = 'R'

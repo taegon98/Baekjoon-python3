a6ddfd336597dbf9d42a1b3e7e84f369ec314334
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-05 13:08:20 +0900

    [Gold IV] Title: 알고스팟, Time: 156 ms, Memory: 116500 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -3,33 +3,32 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N,M = map(int,input().split())
-graph = [list(map(int,input().rstrip())) for _ in range(M)]
-visited = [[-1 for _ in range(N)] for _ in range(M)]
+M,N = map(int,input().split())
+graph = [list(map(int,input().rstrip())) for _ in range(N)]
+visited = [[-1 for _ in range(M)] for _ in range(N)]
 
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
 def bfs(x,y):
+    visited[x][y] = 0
     queue = deque()
-    queue.append([x,y])
 
+    queue.append([x,y])
     while queue:
-        X,Y= queue.popleft()
-        if X== M-1 and Y == N-1:
+        X,Y = queue.popleft()
+        if X == N-1 and Y == M-1:
             print(visited[X][Y])
             exit(0)
         for i in range(4):
             NX = X+dr[i]
             NY = Y+dc[i]
 
-            if 0<=NX<M and 0<=NY<N and visited[NX][NY] == -1:
+            if 0<=NX<N and 0<=NY<M and visited[NX][NY] == -1:
                 if graph[NX][NY] == 0:
                     visited[NX][NY] = visited[X][Y]
-                    queue.appendleft([NX, NY])
+                    queue.appendleft([NX,NY])
                 elif graph[NX][NY] == 1:
                     visited[NX][NY] = visited[X][Y] + 1
                     queue.append([NX,NY])
-visited[0][0] = 0
 bfs(0,0)
-print()

b3e5dbecf8a6e1ea1c8abe9072785931fb62cd98
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-06 17:00:41 +0900

    Update A와 B.py

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 S = list(input().rstrip())
 T = list(input().rstrip())
 
-while T:
-    if T[-1] == 'A':
+while T:    #T리스트안에 데이터가 있을때까지 반복
+    if T[-1] == 'A':    #if 'A' -> pop, 옵션1 : 리스트 뒤에 'A'추가
         T.pop()
-    elif T[-1] == 'B':
+    elif T[-1] == 'B':    #if 'B' -> pop and reverse, 옵션2 : 뒤집고 리스트 뒤에 'B'추가
         T.pop()
         T.reverse()
-    if T==S:
+    if T==S:    #같은 경우면 1출력 -> 종료
         print(1)
         exit(0)
-print(0)
+print(0)    #같게 될 수 없으면 0출력 -> 종료

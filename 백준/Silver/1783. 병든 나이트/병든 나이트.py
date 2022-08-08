f475d08a332ebee89a20f2d1ab4fd9fde5579c9f
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-08 21:00:06 +0900

    Update 병든 나이트.py

--- 백준/Silver/1783. 병든 나이트/병든 나이트.py
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -4,14 +4,14 @@ input = sys.stdin.readline
 
 N,M = map(int,input().split())
 
-if N == 1:
-    print(1)
-elif N == 2:
-    print(min(4, (M+1)//2))
-else:
-    if M < 4:
+if N == 1:    #세로 길이가 1이면
+    print(1)    #처음 시작지점 1개
+elif N == 2:    #세로 길이가 2이면
+    print(min(4, (M+1)//2))    
+else:    #세로 길이가 3이상인 경우
+    if M < 4:    #가로길이가 4미만인 경우
         print(M)
-    elif M < 7:
+    elif M < 7:    #가로길이가 7미만인 경우
         print(4)
-    else:
+    else:    #가로길이가 7이상인 경우
         print(M-2)

2f5810add2fe97216ba96aac8e2424679be4a94b
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-08 20:59:35 +0900

    Update 행렬.py

--- 백준/Silver/1080. 행렬/행렬.py
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -8,14 +8,15 @@ count = 0
 
 for i in range(0, N-2):
     for j in range(M-2):
-        if A[i][j] != B[i][j]:
+        if A[i][j] != B[i][j]:    #값이 다르면
             for k in range(i,i+3):
                 for l in range(j,j+3):
-                    if A[k][l] == 0: A[k][l] = 1
+                    if A[k][l] == 0: A[k][l] = 1    #3x3행렬의 전체 값 reverse
                     else: A[k][l] = 0
-            count += 1
+            count += 1    #전체 값을 reverse할때마다 count값 증가
 
-if A == B:
-    print(count)
-else:
-    print(-1)
+if A == B:    #A행렬과 B행렬이 같으면
+    print(count)    
+    exit(0)
+
+print(-1)    #같지 않으면

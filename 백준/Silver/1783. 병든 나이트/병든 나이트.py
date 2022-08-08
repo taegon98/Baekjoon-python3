71900f4c9f79f24e321bef1388f2a0f280fc7eed
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-08 14:34:45 +0900

    [Silver IV] Title: 병든 나이트, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/1783. 병든 나이트/병든 나이트.py
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -7,14 +7,7 @@ N,M = map(int,input().split())
 if N == 1:
     print(1)
 elif N == 2:
-    if M > 6:
-        print(4)
-    elif M >= 5:
-        print(3)
-    elif M >= 3:
-        print(2)
-    else:
-        print(1)
+    print(min(4, (M+1)//2))
 else:
     if M < 4:
         print(M)

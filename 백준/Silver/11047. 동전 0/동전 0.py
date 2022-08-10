22663868e64be775036538d4d6d86305678b0a1e
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-10 13:46:38 +0900

    Update 동전 0.py

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,16 @@
 N,K = map(int,input().split())
 coin = []
 count = 0
+
 for i in range(N):
     coin.append(int(input()))
-coin.reverse()
+
+coin.reverse()    #가장 큰 지폐부터이용 -> 동전 갯수 최소화
 
 for i in coin:
-    if K // i > 0:
-        count+=(K //i)
-        K = K % i
-    if K == 0:
+    if K // i > 0:    #나누어지면
+        count+=(K //i)    #나누어진 몫을 증감
+        K = K % i    #나누어진 나머지로 K값 갱신
+    if K == 0:    #다 나누어졌으면
         print(count)
         exit(0)

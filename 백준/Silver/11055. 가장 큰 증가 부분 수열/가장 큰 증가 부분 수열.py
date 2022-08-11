6f3a5488db6512627b9310c905c7bbf2e2ac5621
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-11 17:50:52 +0900

    Update 가장 큰 증가 부분 수열.py

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 l = list(map(int, input().split()))
-dp = l[:]
+dp = l[:]    #dp table
 
-for i in range(1, N):
-    for j in range(0,i):
+for i in range(1, N):    #인덱스 1 ~ 마지막 인덱스까지
+    for j in range(0,i):    #처음부터 i인덱스 전까지
         if l[i] > l[j] and dp[i] < dp[j]+l[i]:
             dp[i] = dp[j]+l[i]
 print(max(dp))

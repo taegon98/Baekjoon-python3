2e94568049963ba4c5e097f1ba91b5097b5c87d1
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-15 16:41:03 +0900

    Update 기타 레슨.py

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -4,21 +4,21 @@ input = sys.stdin.readline
 N,M = map(int, input().split())
 l = list(map(int,input().split()))
 
-start, end = max(l), sum(l)
+start, end = max(l), sum(l)    #최소크기(최대갯수), 최대크기(최소갯수)
 
 while start<=end:
     mid = (start+end) // 2
 
     volume = 0
-    disk = 1
+    disk = 1    #한개부터 시작
     for i in range(N):
         volume += l[i]
-        if volume > mid:
-            disk+=1
-            volume = l[i]
-    if disk > M:
-        start = mid + 1
-    elif disk <= M:
-        answer = mid
-        end = mid - 1
+        if volume > mid:    #블루레이 크기를 넘어서면
+            disk+=1    #블루레이 갯수 증가
+            volume = l[i]    #크기 넘어선 강의부터 다시 누적
+    if disk > M:    #갯수가 더 많으면
+        start = mid + 1    #크기 증가
+    elif disk <= M:    #갯수가 더 작거나 같으면
+        answer = mid    
+        end = mid - 1    #크기 감소
 print(answer)

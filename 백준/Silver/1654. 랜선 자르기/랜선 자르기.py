e822d84d7e846e53b9f724c42c5e302a961d3c3c
taegon98 <102223636+taegon98@users.noreply.github.com>
2022-08-16 13:29:47 +0900

    Update 랜선 자르기.py

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -6,17 +6,17 @@ l = []
 for i in range(K):
     l.append(int(input().rstrip()))
 
-start, end = 1, max(l)
+start, end = 1, max(l)    #최대로 가져갈 때, 최소로 가져갈 때
 
 while start<=end:
     mid = (start+end) // 2
 
     LAN = 0
-    for i in range(K):
+    for i in range(K):    #mid크기로 몇개로 나눌 수 있는지 저장
         LAN += (l[i] // mid)
-    if LAN < N:
-        end = mid-1
-    elif LAN >= N:
+    if LAN < N:    #원하는 갯수보다 적으면
+        end = mid-1    #갯수 늘려야함 -> end감소(최대쪽으로)
+    elif LAN >= N:    #원하는 갯수보다 많거나 같으면
         answer = mid
-        start = mid + 1
+        start = mid + 1    #갯수 줄여야함 -> start증가(최소쪽으로)
 print(answer)
